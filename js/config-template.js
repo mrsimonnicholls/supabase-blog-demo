2c9b853b154b1a292f4ed467ee3b24895bff46f6
Simon Nicholls <simon.nicholls@mediadesignschool.com>
2026-06-09 15:41:51 +1200

    update

---
+++ js/config-template.js
@@ -0,0 +1,12 @@
+// FYI This setup is NOT production ready, just for demonstration purposes!
+// DO NOT expose keys in your html or javascript files
+
+// Replace supabaseUrl and supabaseKey with your URL and key. Rename this file to config.js before your next commit!
+// The config.js file WILL NOT be uploaded to your Github repo for security reasons. 
+
+export const config = {
+
+    supabaseUrl: "replace with your URL",
+    supabaseKey: "replace with your Key"
+
+};
